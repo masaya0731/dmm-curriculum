7119a9f82d4d11f3a9f553f3e3cc5b5a685e9fe2
masaya0731 <92997241+masaya0731@users.noreply.github.com>
2022-02-21 13:34:19 +0000

    [Finish] ruby-chapter2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   root to: 'homes#top'
+  # HTML
   resources :html, only: %i[index]
   get '/html/responsive', to: 'html#responsive'
+  
+  # Ruby
+  resources :ruby, only: %i[index]
 
 end
