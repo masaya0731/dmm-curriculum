99e5323ddda6e7a2ca2bc0a4bdfa571db5995f51
masaya0731 <92997241+masaya0731@users.noreply.github.com>
2022-02-22 14:31:25 +0000

    [Update]

---
+++ app/controllers/rails_reviews_controller.rb
@@ -0,0 +1,24 @@
+class RailsReviewsController < ApplicationController
+
+  def top
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
