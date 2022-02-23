71fbf6813791c06aa5b07bc7c554c04eedb68671
masaya0731 <92997241+masaya0731@users.noreply.github.com>
2022-02-23 09:42:33 +0000

    [Finish] Bookers1

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,40 @@
 class ReviewsController < ApplicationController
 
-  def first
-  end
-
   def index
+    @post = ReviewPost.new
+    @posts = ReviewPost.all
   end
 
   def create
+    post = ReviewPost.new(review_post_params)
+    post.save
+    redirect_to review_path(post.id)
   end
 
   def show
+    @post = ReviewPost.find(params[:id])
   end
 
   def edit
+    @post = ReviewPost.find(params[:id])
   end
 
   def update
+    post = ReviewPost.find(params[:id])
+    post.update(review_post_params)
+    redirect_to review_path(post.id)
   end
 
   def destroy
+    post = ReviewPost.find(params[:id])
+    post.destroy
+    redirect_to reviews_path
   end
+
+  private
+
+  def review_post_params
+    params.require(:review_post).permit(:title, :body)
+  end
+
 end
