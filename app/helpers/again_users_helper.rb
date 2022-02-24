3d8e2e6a745647db857c04aff94e634a857ea9bc
masaya0731 <92997241+masaya0731@users.noreply.github.com>
2022-02-24 17:03:35 +0000

    [Add] Bookers2準備

---
+++ app/helpers/again_users_helper.rb
@@ -0,0 +1,2 @@
+module AgainUsersHelper
+end
---
+++ test/controllers/again_books_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AgainBooksControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
